f554fa7de04a5a3b242509bc779ce6244f2949d4
Luwam Mogos <luwam.mogosw@gmail.com>
2023-11-12 00:57:28 -0700

    Added Picture

---
+++ myNewReact/my-app/src/home.jsx
@@ -0,0 +1,14 @@
+//import "./styles.css";
+export default function () {
+  return (
+    <div>
+    <img src="/it's potty time_1.jpg" alt="My Image" />
+    <h1>Your React Component</h1>
+    {/* Other content goes here */}
+  
+      <h1>Home</h1>
+
+      Home Sweet Home
+      </div>
+  );
+}
